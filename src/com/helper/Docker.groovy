55acfaff6a70cf1c641d7ce04fcfba7bdf6ffa1c
Thuc Tran <thuc.tv@techchain.vn>
2022-10-13 00:02:01 +0700

    feat: create docker

---
+++ src/com/helper/Docker.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/env groovy
+
+package com.helper
+
+class Docker implements Serializable {
+	def script
+	Docker(script) {
+		this.script = script
+	}
+
+	def build(String imageName, String creId) {
+		script.echo "Building app ... with $script.BRANCH_NAME";
+		script.withCredentials([
+			usernamePassword(
+				credentialsId: creId,
+				usernameVariable: 'USERNAME',
+				passwordVariable: 'PASSWORD'
+			)
+		]) {
+			script.sh "docker build -t $imageName ."
+			script.sh "echo $script.PASSWORD | docker login -u $script.USERNAME --password-stdin"
+			script.sh "docker push $imageName"
+		}
+	}
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,16 +1,7 @@
 #!/usr/bin/env groovy
 
+import com.helper.Docker
+
 def call(String imageName, String creId) {
-    echo "Building app ... with $BRANCH_NAME";
-    withCredentials([
-        usernamePassword(
-                credentialsId: creId,
-                usernameVariable: 'USERNAME',
-                passwordVariable: 'PASSWORD'
-        )
-    ]) {
-        sh "docker build -t $imageName ."
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh "docker push $imageName"
-    }
+	return new Docker(this).build(imageName, creId)
 }
