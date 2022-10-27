4ee02981989e45ed7b6d2146d8dd63c1028dd000
Thuc Tran <thuc.tv@techchain.vn>
2022-10-27 23:52:25 +0700

    feat: update logic build image

--- src/com/helper/Docker.groovy
+++ src/com/helper/Docker.groovy
@@ -19,7 +19,8 @@ class Docker implements Serializable {
 		]) {
 			script.sh "docker build -t $imageName -t $imageName:$version . "
 			script.sh "echo $script.PASSWORD | docker login -u $script.USERNAME --password-stdin"
-			script.sh "docker push $imageName --all-tags"
+			script.sh "docker push $imageName"
+			script.sh "docker push $imageName:$version"
 			script.echo "Pushed image to dockerhub"
 		}
 	}
